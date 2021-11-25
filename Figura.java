a52173c6c3e80ce4f681a85d956dad0e3ab0fe9f
Badwolffn <luckas.a.2016@gmail.com>
2021-11-25 14:48:42 -0300

    p2

---
+++ Figura.java
@@ -0,0 +1,11 @@
+// Exercício 8
+public abstract interface Figura {
+	
+	public abstract String getNomeFigura();
+	
+	public abstract int getArea();
+	
+	public abstract int getPerimetro();
+	
+	public abstract int[] getLado();
+}
